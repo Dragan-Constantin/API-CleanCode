dd59f69d18d78a1a44cdee34bcf3d6e6c487dabc
Dragan Constantin <constantin.dragan@efrei.net>
2023-09-22 16:26:09 +0200

    Added .env support

--- addData.js
+++ addData.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 const Movie = require('./models/movie');
 
-// Replace <user> and <password> by your own username and password to connect to your MongoDB database
+// Replace username, password  and database in the .env file by your
+// own login credentials and database to connect to your MongoDB database
 // Otherwise, you can also replace it by the url to an other database of your own choosing
-mongoose.connect('mongodb+srv://<user>:<password>@cleancode-api.aqnmjeg.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@${process.env.DATABASE}.mongodb.net/?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
